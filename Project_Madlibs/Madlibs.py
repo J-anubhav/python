eb25a6be949c727cfbcf7be58eed1cbe5e458a51
j-anubhav <anubhavjha.dev@gmail.com>
2025-08-07 19:09:51 +0530

    made a fun and simple game using justt the basics

---
+++ Project_Madlibs/Madlibs.py
@@ -0,0 +1,16 @@
+# Madlibs 
+# it is the game where you creat a story 
+# by filing in the blanks with random words
+
+adj1 = input("Enter an adjective (description): ")
+noun1= input("Enter a noun (person ,place , thing): ")
+adj2= input("Enter an adjective (description): ")
+verb1= input("Enter a vereb ending with 'ing': ")
+adj3= input("Enter an adjective (description): ")
+
+
+
+print(f"Toaday I went to a {adj1} zoo.")
+print(f"In a exibit, I saw a {noun1}.")
+print(f"{noun1} was {adj2} and {verb1}")
+print(f"i was {adj3}")
