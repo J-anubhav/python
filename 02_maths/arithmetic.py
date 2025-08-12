217e5ccf20a4e884c6cbcada561b0b8853233e93
j-anubhav <anubhavjha.dev@gmail.com>
2025-08-12 17:31:52 +0530

    repolished the inbuilt function of the math module

---
+++ 02_maths/arithmetic.py
@@ -0,0 +1,37 @@
+friends =5
+
+# friends = friends +1
+# friends += 1
+# friends *= 10
+
+# friends= friends **2
+# reminder = friends % 2
+
+
+#built in function
+
+# x = 3.14
+# z= 10
+# reminder = round(x)
+# y =-4
+
+# result = abs(y)
+
+# result = pow(2,3) 
+# result = max(x,y,z) 
+# result = min(x,y,z) 
+
+# print(result)
+
+
+import math
+# print(math.pi)
+# print(math.e)
+
+# x= 10
+# result = math.sqrt(10)
+x =9.9
+# result =math.ceil(x)
+# result = math.floor(x) 
+
+print(result)
