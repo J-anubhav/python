72c3422b363038d7ca492b44ab1c109a154ec408
j-anubhav <anubhavjha.dev@gmail.com>
2025-08-13 10:05:02 +0530

    refresed the string in python

---
+++ 03_strings/string.py
@@ -0,0 +1,24 @@
+# name = input("enter your name : ")
+
+# print(len(name))
+# result = name.find("A") # for the first find
+# print(result)
+
+# # and for the last find use rfind() meaning reverse find
+
+# Last_Find = name.rfind("a")
+# print(Last_Find)
+
+# name = name.capitalize() # used for the first letter of the string to be capitalized 
+
+# name = name.upper() # used to change all lower case to upper case 
+# name = name.lower() # used for change all upper case to lowe rcase
+
+
+# result = name.isdigit() # tell is the varible have onlly digit 
+# result = name.isalpha() # tell is the varible have only alphabet return flase for white space
+# result = name.isalnum() # tell is the varroble have num and alphabet only 
+# result = name.count('a') # give us the count of specific char
+# result = name.replace('a', " ")
+print(help(str))
+# print(result)
